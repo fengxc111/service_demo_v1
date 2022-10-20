a6d0652c4d254f3364f5a7ae95992e9e09f9044b
fengxc <1143734409@qq.com>
2022-10-20 10:11:27 +0800

    10/19

--- tcpcliserv/tcpcli09.c
+++ tcpcliserv/tcpcli09.c
@@ -1,15 +1,15 @@
-#include "unp.h"
-#include "str_cli11.c"
-#include "sigpipe.c"
+#include	"unp.h"
+#include    "str_cli09.c"
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	int sockfd;
-	struct sockaddr_in servaddr;
+	int					sockfd;
+	struct sockaddr_in	servaddr;
 
 	if (argc != 2)
 		err_quit("usage: tcpcli <IPaddress>");
-	
+
 	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
@@ -17,11 +17,9 @@ int main(int argc, char **argv)
 	servaddr.sin_port = htons(SERV_PORT);
 	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
-	Signal(SIGPIPE, sig_pipe);
-
 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-	str_cli11(stdin, sockfd);
+	str_cli(stdin, sockfd);
 
 	exit(0);
 }
