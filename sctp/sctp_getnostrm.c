7fc469d2a2ce66f2134f971f45947c9f3f3f57c4
fengxc <1143734409@qq.com>
2022-10-26 10:40:56 +0800

    10/26 sctp

---
+++ sctp/sctp_getnostrm.c
@@ -0,0 +1,16 @@
+#include	"unp.h"
+
+int 
+sctp_get_no_strms(int sock_fd,struct sockaddr *to, socklen_t tolen, struct sctp_sndrcvinfo sri)
+{
+	int retsz;
+	struct sctp_status status;
+	retsz = sizeof(status);	
+	bzero(&status,sizeof(status));
+
+	// status.sstat_assoc_id = sctp_address_to_associd(sock_fd,to,tolen);
+	status.sstat_assoc_id = sri.sinfo_assoc_id;
+	Getsockopt(sock_fd,IPPROTO_SCTP, SCTP_STATUS,
+		   &status, &retsz);
+	return(status.sstat_outstrms);
+}
